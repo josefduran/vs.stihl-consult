80d608794a4f448c9996ced02868826102f6bcb1
Gerardo Ramos <57040617+gerardoramos89@users.noreply.github.com>
2021-06-22 10:44:52 -0500

    Update Map
    Update Map

--- src/components/LateralMenu.jsx
+++ src/components/LateralMenu.jsx
@@ -1,18 +1,13 @@
 import React from 'react'
-import { useHistory } from 'react-router';
-export const LateralMenu = () => {
-    const history = useHistory();
 
-    const handleClick = (option) => {        
-        history.push(`/${option}`)
-    };
+export const LateralMenu = () => {
     return (
         <div className="lm_container_menu">
             <div className="lm_container_items">
-                <img src="../../assets/house.png"  alt="house.png" onClick={ ()=> handleClick("index")}/>
-                <img src="../../assets/arrows.png"  alt="arrows.png" onClick={ ()=> handleClick("index")} />
+                <img src="../../assets/house.png"  alt="house.png" />
+                <img src="../../assets/arrows.png"  alt="arrows.png" />
                 <img src="../../assets/card.png"  alt="card.png" />
-                <img src="../../assets/lupa_.png"  alt="lupa_.png" onClick={ ()=> handleClick("search")}/>
+                <img src="../../assets/lupa_.png"  alt="lupa_.png" />
             </div>
         </div>
     )
