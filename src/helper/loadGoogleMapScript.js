237bbeb232aaf3fbc33192031616403b4053ef01
Gerardo Ramos <coordtss2@gmail.com>
2021-08-25 16:20:57 -0500

    apikey

--- src/helper/loadGoogleMapScript.js
+++ src/helper/loadGoogleMapScript.js
@@ -5,7 +5,7 @@ export const loadGoogleMapScript = (callback) => {
         callback();
     } else {
         const googleMapScript = document.createElement("script");
-        googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${KEY}&libraries=places`;
+        googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyBt5JUH5lB6s8lxjGlh6surkokQdy5ZpAo&libraries=places`;
         window.document.body.appendChild(googleMapScript);
         googleMapScript.addEventListener("load", callback);
     }
