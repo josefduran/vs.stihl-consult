6cfa0227b2a51b15b46f4833540a6c20ab70a6d0
Gerardo Ramos <coordtss2@gmail.com>
2021-09-14 09:35:55 -0500

    Ajsutes

--- src/helper/loadGoogleMapScript.js
+++ src/helper/loadGoogleMapScript.js
@@ -5,7 +5,8 @@ export const loadGoogleMapScript = (callback) => {
         callback();
     } else {
         const googleMapScript = document.createElement("script");
-        googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyBt5JUH5lB6s8lxjGlh6surkokQdy5ZpAo&libraries=places`;
+        const key = process.env.REACT_APP_KEY_PROD;
+        googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${key}&libraries=places`;
         window.document.body.appendChild(googleMapScript);
         googleMapScript.addEventListener("load", callback);
     }
